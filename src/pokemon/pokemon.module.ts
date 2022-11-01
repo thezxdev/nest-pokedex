d1aac5ee5399918207adc489139672a2c4557f52
thezxdev <thezxdev@gmail.com>
2022-10-31 17:16:52 -0700

    Fin seccion 7

---
+++ src/pokemon/pokemon.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name, // entity
+        schema: PokemonSchema,
+      }
+    ])
+  ],
+  controllers: [
+    PokemonController
+  ],
+  providers: [
+    PokemonService
+  ]
+})
+export class PokemonModule {}
